eccd02d6c4a6fefd38c459a93445dda675fa67cd
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-09-30 22:03:53 +0300

    Some temporary mypy cheates

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-
 DB_HOST = "localhost"
 DB_PORT = 5432
 DB_USER = "postgres"
