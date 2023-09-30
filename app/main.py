6be0a2cdea0fd2d529cd3887592c29484c8a4345
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-09-30 21:44:15 +0300

    Some experements with pydantic, SQLAlchemy setup, Hotels model created

--- app/main.py
+++ app/main.py
@@ -1,8 +1,39 @@
-from fastapi import FastAPI
+from dataclasses import dataclass
+from datetime import date
+from typing import Optional
+
+from fastapi import Depends, FastAPI, Query
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/hotels")
-def get_hotels():
-    return "Отель Бридж 5 звёзд"
+@dataclass
+class HotelsSearchArgs:
+    location: str
+    date_from: date
+    date_to: date
+    has_spa: Optional[bool] = None
+    stars: Optional[int] = Query(None, ge=1, le=5)
+
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+
+
+@app.get("/hotels/")
+def get_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
