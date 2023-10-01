44fbcdadb9d74089045556ed78fa345c87d521d2
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-10-01 20:44:09 +0300

    Alembic setup, migration: hotels

--- app/database.py
+++ app/database.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 DB_HOST = "localhost"
 DB_PORT = 5432
-DB_USER = "postgres"
-DB_PASS = "postgres"
-DB_NAME = "postgres"
+DB_USER = "hotels"
+DB_PASS = "hotels"
+DB_NAME = "hotels"
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,11 +1,20 @@
+import sys
 from logging.config import fileConfig
+from os.path import dirname, abspath
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from app.database import Base, DATABASE_URL
+from app.hotels.models import Hotels
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", f"{DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -16,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
