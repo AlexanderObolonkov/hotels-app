27e257e0735620aa8169a116f8ced8aa6614e7b2
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-10-06 20:13:13 +0300

    Booking schema added, some bug fixes

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import RowMapping, select
+from sqlalchemy import select
 
 from app.database import Base, async_session_maker
 
@@ -9,15 +9,15 @@ class BaseDAO:
     model: Optional[type[Base]] = None
 
     @classmethod
-    async def find_one_or_none(cls, **filter_by) -> Optional[RowMapping]:
+    async def find_one_or_none(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select().filter_by(**filter_by)
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().one_or_none()
 
     @classmethod
-    async def find_all(cls, **filter_by) -> Optional[RowMapping]:
+    async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
-            query = select().filter_by(**filter_by)
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
             result = await session.execute(query)
-            return result.mappings().one_or_none()
+            return result.mappings().all()
