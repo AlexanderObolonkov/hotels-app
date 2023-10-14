78a19d79aa99b49b8829df41264002258cc4cfbd
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-10-14 21:49:03 +0300

    Main cleared, bookings add override

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from datetime import date
 from typing import Optional
 
 from fastapi import Depends, FastAPI, Query
-from pydantic import BaseModel
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
@@ -23,23 +22,6 @@ class HotelsSearchArgs:
     stars: Optional[int] = Query(None, ge=1, le=5)
 
 
-class SHotel(BaseModel):
-    address: str
-    name: str
-    stars: int
-
-
 @app.get("/hotels/")
 def get_hotels(search_args: HotelsSearchArgs = Depends()):
     return search_args
-
-
-class SBooking(BaseModel):
-    room_id: int
-    date_from: date
-    date_to: date
-
-
-@app.post("/bookings")
-def add_booking(booking: SBooking):
-    pass
