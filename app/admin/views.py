6463b97c56cac5fda1ae416df73afe3c58b79afa
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-10-29 16:23:50 +0300

    CI change

--- app/admin/views.py
+++ app/admin/views.py
@@ -6,7 +6,7 @@ from app.bookings.models import Bookings
 from app.users.models import Users
 
 
-class UsersAdmin(ModelView, model=Users):  # type: ignore
+class UsersAdmin(ModelView, model=Users):
     column_list = [cast(str, Users.id), cast(str, Users.email)]
     column_details_exclude_list = [cast(str, Users.hashed_password)]
     can_delete = False
@@ -15,7 +15,7 @@ class UsersAdmin(ModelView, model=Users):  # type: ignore
     icon = "fa-solid fa-user"
 
 
-class BookingsAdmin(ModelView, model=Bookings):  # type: ignore
+class BookingsAdmin(ModelView, model=Bookings):
     column_list = [c.name for c in Bookings.__table__.columns] + [Bookings.user]
     name = "Бронь"
     name_plural = "Брони"
