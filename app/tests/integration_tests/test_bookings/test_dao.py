49013c7852d14bcc5276e6376ca5fb1c235b4147
Alexandr Obolonkov <itbolonkov2019@gmail.com>
2023-11-25 19:34:45 +0300

    Bookings CRUD integration test added

--- app/tests/integration_tests/test_bookings/test_dao.py
+++ app/tests/integration_tests/test_bookings/test_dao.py
@@ -1,5 +1,8 @@
 from datetime import date
 
+import pytest
+from httpx import AsyncClient
+
 from app.bookings.dao import BookingDAO
 
 
@@ -17,3 +20,30 @@ async def test_and_get_booking():
     new_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
 
     assert new_booking is not None
+
+
+@pytest.mark.parametrize(
+    "user_id,room_id",
+    [
+        (2, 2),
+        (2, 3),
+        (1, 4),
+        (1, 4),
+    ],
+)
+async def test_booking_crud(user_id: int, room_id: int, get_async_client: AsyncClient):
+    new_booking = await BookingDAO.add(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=date(2023, 11, 25),
+        date_to=date(2023, 12, 21),
+    )
+    assert new_booking.user_id == user_id
+    assert new_booking.room_id == room_id
+
+    added_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
+    assert added_booking is not None
+
+    await BookingDAO.delete(id=added_booking.id, user_id=user_id)
+    deleted_booking = await BookingDAO.find_one_or_none(id=added_booking.id)
+    assert deleted_booking is None
